b803804cc576740608f25987e31e2b71adacd72e
daniMend <50723675+daniMend@users.noreply.github.com>
2020-10-16 16:33:09 -0300

    AjusteUsuário

--- src/main/java/br/com/silo/backoffice/dao/UsuarioDAO.java
+++ src/main/java/br/com/silo/backoffice/dao/UsuarioDAO.java
@@ -14,7 +14,7 @@ public interface UsuarioDAO extends PagingAndSortingRepository<Usuario, Long> { 
     @Query("SELECT u FROM Usuario u WHERE u.nome = ?1")
     Usuario findByUsername(String nome);
 
-    @Query(nativeQuery = true, value = "SELECT u.id,u.nome,e.empresa_nome FROM usuario u JOIN empresa e ON u.empresa_id=e.id WHERE nome LIKE concat('%', ?1,'%');")
+    @Query(nativeQuery = true, value = "SELECT * FROM usuario u JOIN empresa e ON u.empresa_id=e.id WHERE nome LIKE concat('%', ?1,'%')")
     Page<Usuario> findPaged(String nome, Pageable pageable);
 
     //@Query(nativeQuery = true, value = "SELECT * FROM usuario WHERE nome LIKE concat('%', ?1,'%')")
